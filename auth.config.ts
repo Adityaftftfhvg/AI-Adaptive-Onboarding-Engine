5872c40250d19397fb4ab17d5d08c7ab21654300
Adityaftftfhvg <pathakaditya423@gmail.com>
2026-08-07 14:01:20 +0530

    Create auth.config.ts

---
+++ auth.config.ts
@@ -0,0 +1,16 @@
+import type { NextAuthConfig } from "next-auth";
+
+export const authConfig = {
+  pages: {
+    signIn: "/login",
+  },
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user;
+      const isOnProtectedRoute = !nextUrl.pathname.startsWith("/login") && !nextUrl.pathname.startsWith("/signup");
+      if (isOnProtectedRoute) return isLoggedIn;
+      return true;
+    },
+  },
+  providers: [], // no providers here — filled in in auth.ts
+} satisfies NextAuthConfig;
