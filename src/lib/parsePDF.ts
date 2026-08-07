7abcd5c43b2013b6278b3e3f2788f3720559692c
Adityaftftfhvg <pathakaditya423@gmail.com>
2026-08-07 21:24:29 +0530

    Update parsePDF.ts

--- src/lib/parsePDF.ts
+++ src/lib/parsePDF.ts
@@ -1,25 +1,7 @@
-import PDFParser from "pdf2json";
-
-
-
-
+import { extractText, getDocumentProxy } from "unpdf";
 
 export async function extractTextFromPDF(buffer: Buffer): Promise<string> {
-  return new Promise((resolve, reject) => {
-    const pdfParser = new (PDFParser as any)(null, 1);
-
-    pdfParser.on("pdfParser_dataError", (err: any) => {
-      reject(new Error("PDF parsing failed: " + err.parserError));
-    });
-
-
-
-
-    pdfParser.on("pdfParser_dataReady", () => {
-      const text = pdfParser.getRawTextContent();
-      resolve(text);
-    });
-
-    pdfParser.parseBuffer(buffer);
-  });
+  const pdf = await getDocumentProxy(new Uint8Array(buffer));
+  const { text } = await extractText(pdf, { mergePages: true });
+  return text;
 }
