e141f9054c7c0dcd17da363f721e8b7fcc85697e
Aditya Pathak <pathakaditya423@gmail.com>
2026-08-07 19:04:47 +0530

    Fix submodule issue - track VirtualCourses files properly

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,8 @@ const nextConfig: NextConfig = {
   env: {
     HF_API_KEY: process.env.HF_API_KEY,
   },
-
   turbopack: {},
+
   serverExternalPackages: ["unpdf", "pdf2json", "@xenova/transformers", "@prisma/client"],
 };
 
