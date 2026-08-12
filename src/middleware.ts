a5a2311651f056ed7340a87e9d9e266d7592f84f
Adityaftftfhvg <pathakaditya423@gmail.com>
2026-08-12 14:49:18 +0530

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,1 +1,10 @@
 
+// src/middleware.ts
+import NextAuth from "next-auth";
+import { authConfig } from "@/auth.config";
+
+export default NextAuth(authConfig).auth;
+
+export const config = {
+  matcher: ["/resume-builder/:path*"],
+};
